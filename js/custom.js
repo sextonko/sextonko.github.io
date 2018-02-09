c6bbfda1fb21ec4706048b4593819308e143d79c
Katie Sexton <katie@burke.design>
2018-02-08 19:40:01 -0500

    wrong code
    nope

--- js/custom.js
+++ js/custom.js
@@ -1,21 +1,5 @@
 $(document).ready(function() {
 
-  $(".otherpages, .bottom").each(function(){
-       $(this).hide();
-      if($(this).attr('id') == 'main') {
-          $(this).show();
-      }
-  });
 
-  $('a').on( "click", function(e) {
-      e.preventDefault();
-      var id = $(this).attr('data-related');
-      $("div").each(function(){
-          $(this).hide();
-          if($(this).attr('id') == id) {
-              $(this).show();
-          }
-      });
-  });
 
 });
