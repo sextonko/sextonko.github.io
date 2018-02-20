c67e371765d5ed622a243c78c96409a928f6630f
Katie Sexton <katie@burke.design>
2018-02-19 19:44:47 -0500

    performant wrong
    incorrect

--- js/custom.js
+++ js/custom.js
@@ -9,27 +9,25 @@ $(document).ready(function() {
 
   $('a').on("click", function(e) {
     e.preventDefault();
-    var $this = $(this);
-    var id = $this.attr('data-related');
+    var id = $(this).attr('data-related');
     $("div.otherpages").each(function() {
-      $this.hide();
-      if ($this.attr('id') == id) {
-        $this.show();
-        var $parent = $this.parent();
-        if ($this.attr('id') == 'contact') {
-          $parent.css({
+      $(this).hide();
+      if ($(this).attr('id') == id) {
+        $(this).show();
+        if ($(this).attr('id') == 'contact') {
+          $(this).parent().css({
             'background-color': '#92a8d1'
           });
-        } else if ($this.attr('id') == 'about') {
-          $parent.css({
+        } else if ($(this).attr('id') == 'about') {
+          $(this).parent().css({
             'background-color': '#b0dbd1'
           });
-        } else if ($this.attr('id') == 'resume') {
-          $parent.css({
+        } else if ($(this).attr('id') == 'resume') {
+          $(this).parent().css({
             'background-color': '#cec5e0'
           });
         } else {
-          $parent.css({
+          $(this).parent().css({
             'background-color': '#fccfcf'
           });
         }
