fffe9d9560408971ee2173f11631f2c964d5802b
Katie Sexton <katie@burke.design>
2018-02-19 19:30:11 -0500

    beautified
    jQuery messed up brackets

--- js/custom.js
+++ js/custom.js
@@ -1,25 +1,31 @@
 $(document).ready(function() {
 
-  $(".otherpages, .bottom").each(function(){
-       $(this).hide();
-      if($(this).attr('id') == 'main') {
-          $(this).show();
+  $(".otherpages, .bottom").each(function() {
+    $(this).hide();
+    if ($(this).attr('id') == 'main') {
+      $(this).show();
+    }
+  });
+
+  $('a').on("click", function(e) {
+    e.preventDefault();
+    var id = $(this).attr('data-related');
+    $("div.otherpages").each(function() {
+      $(this).hide();
+      if ($(this).attr('id') == id) {
+        $(this).show();
+        if ($(this).attr('id') == 'contact') {
+          $(this).parent().css({'background-color':'#92a8d1'});
+        } else if ($(this).attr('id') == 'about') {
+          $(this).parent().css({'background-color':'#b0dbd1'});
+        } else if ($(this).attr('id') == 'resume') {
+          $(this).parent().css({'background-color':'#88B04B'});
+        }
       }
+    });
   });
 
-  $('a').on( "click", function(e) {
-      e.preventDefault();
-      var id = $(this).attr('data-related');
-      $("div.otherpages").each(function(){
-          $(this).hide();
-          if($(this).attr('id') == id) {
-              $(this).show();
-              $(this).parent().toggleClass('blue');
-          }
-      });
-  });
-
-  $('.otherpages').each(function(){
+  $('.otherpages').each(function() {
 
   });
 
