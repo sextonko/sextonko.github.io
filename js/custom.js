ac6a2000d02cac320c1ca4ee384c966a4e5ffad0
Katie Sexton <katie@burke.design>
2018-02-19 19:43:21 -0500

    performance
    jQuery

--- js/custom.js
+++ js/custom.js
@@ -9,20 +9,29 @@ $(document).ready(function() {
 
   $('a').on("click", function(e) {
     e.preventDefault();
-    var id = $(this).attr('data-related');
+    var $this = $(this);
+    var id = $this.attr('data-related');
     $("div.otherpages").each(function() {
-      $(this).hide();
-      if ($(this).attr('id') == id) {
-        $(this).show();
-        if ($(this).attr('id') == 'contact') {
-          $(this).parent().css({'background-color':'#92a8d1'});
-        } else if ($(this).attr('id') == 'about') {
-          $(this).parent().css({'background-color':'#b0dbd1'});
-        } else if ($(this).attr('id') == 'resume') {
-          $(this).parent().css({'background-color':'#cec5e0'});
-        }
-        else {
-          $(this).parent().css({'background-color':'#fccfcf'});
+      $this.hide();
+      if ($this.attr('id') == id) {
+        $this.show();
+        var $parent = $this.parent();
+        if ($this.attr('id') == 'contact') {
+          $parent.css({
+            'background-color': '#92a8d1'
+          });
+        } else if ($this.attr('id') == 'about') {
+          $parent.css({
+            'background-color': '#b0dbd1'
+          });
+        } else if ($this.attr('id') == 'resume') {
+          $parent.css({
+            'background-color': '#cec5e0'
+          });
+        } else {
+          $parent.css({
+            'background-color': '#fccfcf'
+          });
         }
       }
     });
