8d7b4386394de859ce0d256b7965a9509f3a8856
Katie Sexton <katie@burke.design>
2018-02-28 16:35:48 -0500

    title fun
    fun title change

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,22 @@
 $(document).ready(function() {
 
+  $(function() {
+    var message = "Don't forget me!";
+    var original;
+
+    $(window).focus(function() {
+      if (original) {
+        document.title = original;
+      }
+    }).blur(function() {
+      var title = $('title').text();
+      if (title != message) {
+        original = title;
+      }
+      document.title = message;
+    });
+  });
+
   $(".otherpages, .bottom").each(function() {
     $(this).hide();
     if ($(this).attr('id') == 'main') {
